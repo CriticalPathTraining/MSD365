d9839e9d20f17d21c60a099cc72cf3d44ce776b8
Ted Pattison <ted@tedpattison.net>
2018-09-08 08:57:00 -0400

    Updates

---
+++ Modules/01_GettingStarted/Lab/AddWingtipProductsList/AddWingtipProductsList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AddWingtipProductsList {
+
+	class Program {
+
+		static void Main() {
+
+      Console.WriteLine("Adding Team Site Content");
+      Console.WriteLine();
+
+      WingtipContentGenerator.CreateProductCategoriesTermset();
+      WingtipContentGenerator.CreateProductsLists();
+
+      Console.WriteLine();
+      Console.WriteLine("The program has finsihed. Press ENTER to close this window");
+      Console.WriteLine();
+      Console.ReadLine();
+			
+    }
+		
+  }
+}
