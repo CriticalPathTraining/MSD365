c4252f00c2ce38dc9c44aca5df3bf5c41a4a37de
Ted Pattison <ted@tedpattison.net>
2018-11-15 07:32:33 -0500

    Updates

---
+++ Modules/10_MicrosoftTeams/Lab/Solution/03 - teams-app1/src/app/scripts/client.ts
@@ -0,0 +1,6 @@
+// Automatically added for the teamsApp1Tab tab
+export * from "./teamsApp1Tab";
+export * from "./teamsApp1TabConfig";
+export * from "./teamsApp1TabRemove";
+export * from './adminconsent';
+export * from './auth';
