fc230b85f29a081edd7fc5dfd123a64b9dabf2bb
Ted Pattison <ted@tedpattison.net>
2019-04-23 09:56:47 -0400

    Updates

--- Modules/03_React/Demo/react-bean-counter/README.md
+++
@@ -1,2 +0,0 @@
-# The React Starter Project
-**react-starter** is a demo starter project for developing with React.js, TypeScript and Webpack.
--- Modules/03_React/Demo/react-starter/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
-
-var topLevelAppComponent = <App />;
-var target = document.getElementById('react-target');
-
-render(topLevelAppComponent, target);
