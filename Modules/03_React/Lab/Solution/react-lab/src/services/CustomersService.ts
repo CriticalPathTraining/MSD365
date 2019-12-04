f1782a66333b9432401c0af98d947dd68a6e29e8
Ted Pattison <ted@tedpattison.net>
2019-12-04 11:26:17 -0500

    Updates

---
+++ Modules/03_React/Lab/Solution/react-lab/src/services/CustomersService.ts
@@ -0,0 +1,46 @@
+import ICustomer from "../models/ICustomer";
+import ICustomerDetail from "../models/ICustomerDetail";
+import ICustomersService from "../models/ICustomersService";
+
+export default class CustomersService implements ICustomersService {
+
+    getCustomers(): Promise<ICustomer[]> {
+
+        const restUrl =
+          "http://subliminalsystems.com/api/Customers/?" +
+          "$select=CustomerId,LastName,FirstName,EmailAddress,WorkPhone,HomePhone,Company" +
+          "&$filter=(CustomerId+le+12)&$top=200";
+      
+        return fetch(restUrl)
+          .then(response => response.json())
+          .then(response => {
+            return response.value;
+          });
+      }
+      
+      getCustomersByLastName = (lastNameSearch: string): Promise<ICustomer[]> => {
+
+        const restUrl =
+          "http://subliminalsystems.com/api/Customers/?" +
+          "$select=CustomerId,LastName,FirstName,EmailAddress,WorkPhone,HomePhone,Company" +
+          `&$filter=startswith(tolower(LastName),tolower('${lastNameSearch}'))&$orderby=LastName,FirstName`;
+      
+        return fetch(restUrl)
+          .then(response => response.json())
+          .then(response => {
+            return response.value;
+          });
+      }
+      
+      getCustomer = (customerId: string): Promise<ICustomerDetail> => {
+
+        const restUrl = "http://subliminalsystems.com/api/Customers(" + customerId + ")";
+      
+        return fetch(restUrl)
+          .then(response => response.json())
+          .then(response => {
+            return response;
+          });
+      }
+      
+}
