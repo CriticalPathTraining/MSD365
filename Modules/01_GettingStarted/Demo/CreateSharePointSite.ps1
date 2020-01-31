05dbeeb0d920330411680e3624ae303b46e3267d
Ted Pattison <ted@tedpattison.net>
2020-01-31 14:41:13 -0500

    Updates

---
+++ Modules/01_GettingStarted/Demo/CreateSharePointSite.ps1
@@ -0,0 +1,20 @@
+# connect to tenant admin sire collection
+$urlSharePointAdmin = "https://msd365tenant-admin.sharepoint.com"
+Connect-SPOService -Url $urlSharePointAdmin
+
+# create new SharePoint team site
+$urlNewSite = "https://msd365tenant.sharepoint.com/sites/teamsite2"
+$owner = "student@msd365tenant.onMicrosoft.com"
+$quota = 0
+$siteTitle = "Team Site 2"
+$template = "STS#3"
+New-SPOSite -Url $urlNewSite -Owner $owner -Title $siteTitle -StorageQuota $quota -Template $template
+
+# get info about new team site
+Get-SPOSite -Identity $url | select *
+
+# update title with next text value
+Set-SPOSite -Identity $url -Title "A New and Better Title"
+
+# open new site in browser
+Start-Process $urlNewSite
